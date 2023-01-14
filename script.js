6e2cc2df5545f7af6c38e34b1d133e6975f8ecc1
LucasBento <lubentoneves@gmail.com>
2023-01-14 19:15:24 -0300

    virar cartas pré finalizado

--- script.js
+++ script.js
@@ -3,4 +3,11 @@ let NumeroDeCartas = prompt("Com quantas cartas quer jogar ?");
 while (NumeroDeCartas < 4 || NumeroDeCartas > 14 || (NumeroDeCartas%2) !== 0) {
     NumeroDeCartas = prompt("Com quantas cartas quer jogar ?")
 }
+if (NumeroDeCartas == 14) {
+    let alinhar = document.querySelector("ul");
+    alinhar.classList.add("JogoCom14")
+}
 
+function SelecionadorDeCarta(selecionada){
+    selecionada.classList.add("clicado");
+}
