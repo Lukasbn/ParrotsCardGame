992f8ef800ec40363b3f070dc393b20610a00b26
LucasBento <lubentoneves@gmail.com>
2023-01-14 23:03:13 -0300

    Distribuição das cartas finalizada

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 let NumeroDeCartas = prompt("Com quantas cartas quer jogar ?");
 let ParVirado = [];
 let jogadas = 0
+let imagens = ["bobrossparrot.gif","explodyparrot.gif","fiestaparrot.gif","metalparrot.gif","revertitparrot.gif","tripletsparrot.gif","unicornparrot.gif"];
+let distribudas = [];
+imagens.sort(comparador);
+let contador = 0;
+const tabuleiro = document.querySelector("ul");
+
 
 while (NumeroDeCartas < 4 || NumeroDeCartas > 14 || (NumeroDeCartas%2) !== 0) {
     NumeroDeCartas = prompt("Com quantas cartas quer jogar ?")
@@ -11,11 +17,34 @@ if (NumeroDeCartas == 14) {
     alinhar.classList.add("JogoCom14")
 }
 
+while (distribudas.length < NumeroDeCartas){
+    distribudas.push(imagens[contador])
+    distribudas.push(imagens[contador])
+    contador ++
+}
+
+distribudas.sort(comparador);
+
+for (let index = 0; index < NumeroDeCartas; index++) {
+    tabuleiro.innerHTML += `
+    <li>
+        <div class="card" onclick="SelecionadorDeCarta(this)">
+            <div class="front-face face">
+                <img src="./imagens/back.png" alt="imagen não encontrada">
+            </div>
+            <div class="back-face face">
+                <img src="./imagens/${distribudas[index]}" alt="imagen não encontrada">
+            </div>
+        </div>
+    </li>
+    `
+}
+
+
 function SelecionadorDeCarta(selecionada){
     if (!selecionada.classList.contains("clicado")){
         selecionada.classList.add("clicado");
         ParVirado.push(selecionada);
-        console.log(ParVirado[0].children[1].children[0].getAttribute("src"));
         Comparador();
     }
 }
@@ -28,11 +57,13 @@ function Comparador(){
         }
         ParVirado = [];
         jogadas = jogadas + 2;
-        console.log(jogadas);
     }
 }
 
 function remover(parametro){
-    console.log(parametro);
     parametro.classList.remove("clicado");
+}
+
+function comparador() { 
+	return Math.random() - 0.5; 
 }
